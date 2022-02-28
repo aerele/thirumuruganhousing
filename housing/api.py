cd1b7278a112c46d346c01f5edac1316381399f6
l0gesh29 <logeshperiyasamy24@gmail.com>
2022-02-28 07:57:48 +0000

    Minor changes to collection log

--- housing/api.py
+++ housing/api.py
@@ -1,43 +1,102 @@
 from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
+from datetime import date
+
+
+
 @frappe.whitelist()
-def get_collection_log(dic):
-    if dic['is_rent'] == 'YES':
-        c_log=frappe.new_doc('Collection Log')
-        c_log.serial = dic['serial']
-        c_log.month = dic['month']
-        c_log.year = dic['year']
-        c_log.is_rent = 'YES'
-        c_log.amount = dic['amount']
-        c_log.collected_by = dic['collected_by']
-        c_log.save()
-    if dic['is_advance'] =='YES':
-        c_log  =frappe.new_doc('Collection Log')
-        c_log.serial = dic['serial']
-        c_log.month = dic['month']
-        c_log.year = dic['year']
-        c_log.is_advance = 'YES'
-        c_log.amount = dic['amount']
-        c_log.collected_by = dic['collected_by']
-        c_log.save()
-    if dic['is_others'] == 'YES':
-        c_log=frappe.new_doc('Collection Log')
-        c_log.serial = dic['serial']
-        c_log.month = dic['month']
-        c_log.year = dic['year']
-        c_log.is_others = 'YES'
-        c_log.amount = dic['amount']
-        c_log.collected_by = dic['collected_by']
-        c_log.save()
-   
+def get_transactions():
+	today = date.today()
+	api_key = frappe.request.headers.get('Authorization').split(' ')[1].split(':')[0]
+	user = frappe.db.get_value("User",{'api_key':api_key})
+	list = []
+	#outstanding_rent =[]
+	for i in frappe.db.get_list("Collection Log", {"paid_on": today, "collected_by":user, "docstatus":["!=", 2]}, ["name", "serial", "amount","month","year"]):
+		outstanding=frappe.db.get_value("Tenant Payment",{"serial" : i.serial},"total_outstanding")
+		t=0
+		t1=frappe.db.get_value('Residence',{'serial':i.serial},"Advance")
+		t2=frappe.db.get_value('Residence',{'serial':i.serial},"paid_advance")
+		t=t1-t2
+		list.append({
+				"name":i.name,
+				"serial":i.serial,
+				"customer_name": frappe.db.get_value("Residence", i.serial, "tenant_name"),
+				"customer_mobile_number": frappe.db.get_value("Residence", i.serial, "phone_number"),
+				"paid_due_amount": i.amount,
+				"outstandig":[str(outstanding)+"-"+str(i.month)+"-"+str(i.year),"Advance-"+str(t), "Others"]
+				})
+	return list
+
+@frappe.whitelist()
+def get_collection_log(**dic):
+	try:
+		api_key = frappe.request.headers.get('Authorization').split(' ')[1].split(':')[0]
+		user = frappe.db.get_value("User",{'api_key':api_key})
+		if dic['is_rent'] == 'YES':
+			c_log=frappe.new_doc('Collection Log')
+			c_log.paid_on = date.today()
+			c_log.serial = dic['serial']
+			c_log.month = dic['month']
+			c_log.year = dic['year']
+			c_log.is_rent = 'YES'
+			c_log.amount = dic['amount']
+			c_log.collected_by = user
+			c_log.save()
+			c_log.submit()
+		if dic['is_advance'] =='YES':
+			c_log  =frappe.new_doc('Collection Log')
+			c_log.serial = dic['serial']
+			c_log.paid_on = date.today()
+			c_log.month = dic['month']
+			c_log.year = dic['year']
+			c_log.is_advance = 'YES'
+			c_log.amount = dic['amount']
+			c_log.collected_by = user
+			c_log.save()
+			c_log.submit()
+		if dic['is_others'] == 'YES':
+			c_log=frappe.new_doc('Collection Log')
+			c_log.serial = dic['serial']
+			c_log.paid_on = date.today()
+			c_log.month = dic['month']
+			c_log.year = dic['year']
+			c_log.is_others = 'YES'
+			c_log.amount = dic['amount']
+			c_log.collected_by = user
+			c_log.save()
+			c_log.submit()
+	except Exception as e:
+		traceback = frappe.get_traceback()
+		frappe.log_error(message=traceback)
+@frappe.whitelist()
+def user_validation():
+	return "success"
 
 
+@frappe.whitelist()
+def user_validation():
+	return "success"
 
+@frappe.whitelist()
+def set_detail_to_app(house_no):
+	outstanding_rent =[]
+	a = frappe.db.get_value('Residence',{'serial' : house_no},["tenant_name","phone_number", "area"])
+	if frappe.db.get_value('Tenant Payment',{'serial' : house_no},'total_outstanding'):
+		l=frappe.db.get_list('Tenant Payment',{'serial' :house_no},['outstanding','month','year'])
+		for i in l:
+			s = str(i['outstanding'])+"-"+str(i['month'])+"-"+str(i['year'])
+			if i['outstanding'] > 0:
+				outstanding_rent.append(s)
+	t=0
+	t1=frappe.db.get_value('Residence',{'serial':house_no},"Advance")
+	t2=frappe.db.get_value('Residence',{'serial':house_no},"paid_advance")
+	t=t1-t2
+	if t:
+		outstanding_rent += ["advance-" + str(t)]
+	return [a,outstanding_rent ]
 
-
-
-
-    
-
-
+@frappe.whitelist()
+def cancel_log(logname):
+	doc=frappe.get_doc("Collection Log",logname)
+	doc.cancel()
--- housing/thirumurugan_housing/doctype/collection_log/collection_log.py
+++ housing/thirumurugan_housing/doctype/collection_log/collection_log.py
@@ -9,19 +9,20 @@ from frappe.utils import today
 class CollectionLog(Document):
 	def before_submit(self):
 		if self.is_rent =='YES':
-			frappe.msgprint('success')
 			p_key=frappe.db.get_value('Tenant Payment', {'serial': self.serial, 'month': self.month, 'year': self.year}, 'name')
 			get_amt=frappe.db.get_value('Tenant Payment', {'serial': self.serial, 'month': self.month, 'year': self.year}, 'outstanding')
-			if ((self.amount) > (get_amt)):
+			if not self.amount and (float(self.amount) > (get_amt)):
 				frappe.throw('Amount is Exceeded then Outstanding')
 			t_doc=frappe.get_doc('Tenant Payment', p_key)
-			t_doc.paid_amount=t_doc.paid_amount+self.amount
-			
+			t_doc.paid_amount=t_doc.paid_amount+ float(self.amount)
+			t_doc.paid_on = today()
+			t_doc.collected_by = self.collected_by
 			t_doc.save()
+
 		if  self.is_advance =='YES':
 			p_key=frappe.db.get_value('Residence', {'serial': self.serial})
 			res_doc=frappe.get_doc('Residence', p_key)
-			res_doc.paid_advance=res_doc.paid_advance+self.amount
+			res_doc.paid_advance=res_doc.paid_advance+ float(self.amount)
 			res_doc.save()
 			res_doc.reload()
 		if self.is_others =='YES':
@@ -29,11 +30,8 @@ class CollectionLog(Document):
 	def on_cancel(self):
 		p_key=frappe.db.get_value('Tenant Payment', {'serial':self.serial, 'month':self.month, 'year':self.year}, 'name')
 		t_doc=frappe.get_doc('Tenant Payment', p_key)
-		t_doc.outstanding=t_doc.outstanding+self.amount
-		t_doc.total_outstanding=int(t_doc.total_outstanding)+self.amount
+		t_doc.paid_amount = t_doc.paid_amount - float(self.amount)
 		t_doc.save()
-
-
 
 
 			
